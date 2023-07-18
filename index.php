0ca9823daad94cfb01c4245944e602299ef2b12a
Pushpendra7895 <psharma1832000@gmail.com>
2023-07-18 09:58:40 +0530

    my cagoyal website

---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/js/bootstrap.bundle.min.js"></script>
+     <link rel="stylesheet" href="style.css">
+</head>
+<body>
+ <header>
+    <div class="logo">
+      <a href="#"><img src="images/Logo.png" alt=""></a>
+    </div>
+    <div class="inform">
+     <P><a href="#">
+      <i class="fa fa-phone">&nbsp;&nbsp;<span class="sp">+91-8178883995</span></i></a>
+     &nbsp;&nbsp;&nbsp;
+    <a href="#"> <i class="fa fa-envelope">&nbsp;&nbsp;<span class="sp">ag@caadityagoel.in</span></i>
+     </a></P>
+    </div>
+  </header>
+  <div class="container-fluid">
+    <div class="container">
+    <nav>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About Us</a></li>
+        <li><a href="">Contact Us</a></li>
+        <li><a href="">Services</a></li>
+      </ul>
+    </nav>
+     <h1>Welcome to <span> Aditya Goel & Co,</span><br>Chartered Accountants</h1>
+   </div>
+  </div>
+   <div class="container-fluid-1">
+    <div class="container-1">
+      <div>
+          <h1>About Me</h1>
+          <hr>
+        <p class="about">
+          Aditya Goel & Co, established in the year 2020, is a Noida-based CA Firm.
+        <br>
+      <center> Aditya Goel & Co is a professionally managed firm which offers wide range of professional 
+        services under one roof which includes New Business Registration, Bookkeeping & Accounting, Auditing & Ass
+        urance, Income Tax, Goods & Services Tax (GST), Secretarial Compliances, Trademarks, Financial Analysis & MIS Reporting
+        , Certifications and etc. Our clients include Private Limited Companies, Charitable Trusts, Section 8 Companies (NPO), 
+        Individuals, Partnership Firms & NRI’s. Our primary focus is on value addition to the clients by deliver
+        ing them highest quality of services in an accurate, timely and relevant manner. We are committed towards maintaining 
+        the highest level of integrity, objectivity, confidentiality, professional competence and due care. Our professional approac
+        h blended with personal care has earned us enormous confidence of all our clients, which is reflected in an enduring pr
+        ofessional relationship that we enjoy with them as also in the consistent growth in portfolio and reach of our services
+      </center>
+      </p> 
+     </div>
+    </div>
+      <div class="container-fluid-2">
+      <div class="container-2">
+    <div class="row">
+    <div class="col">
+        <h2><i aria-hidden="true" class="fas fa-star"></i>&nbsp;&nbsp;
+        Value addition</h2>
+        <img src="images/1.jpg" alt="target">
+    </div>
+    <div class="col">
+       <h2><i  aria-hidden="true" class="fas fa-calculator"></i>&nbsp;&nbsp;High Quality Services</h2>
+       <img src="images/3.webp" alt="target">
+
+    </div>
+    <div class="col">
+    <h2><i aria-hidden="true" class="fas fa-business-time"></i>&nbsp;&nbsp;Punchuality</h2>
+    <img src="images/4.jpg" alt="">
+    </div>
+  </div>
+</div>
+<h1>OUR TEAM</h1>
+    <hr>
+<div class="container-fluid-3">
+  <div class="container-3">
+
+<div class="d1">
+      <img src="images/Aditya-Goel.webp" alt="target">
+      <h4>CA Aditya Goel | Founder
+       FCA,B.Com(H)</h4>
+       <p>
+       Aditya Goel is a fellow member of the Institute of Chartered
+        Accountants of India (ICAI), qualified as a Chartered Accountant
+         in the year 2011 and a commerce graduate from Delhi University with 
+         over 12 years of experience. He has worked with professionally managed 
+         corporates in capacity as a consultant. He has rich experience of handling 
+         corporate affairs in different areas for over a decade. He has helped many
+          organizations to set up business in India and operating it smoothly. He has
+           vast knowledge and a thorough understanding of Indian Laws and regulations,
+            as well as the complex taxation system. He has also served large 
+            conglomerates like Wipro, Genpact, Barclays & Deloitte.
+       </p>
+    </div>
+    <div class="d1">
+    <img src="images/manjeet-sharma.webp" alt="target">
+    <h4>Manjeet Sharma | Consultant MBA in Finance</h4>
+      <p>
+      Manjeet Sharma is a highly skilled Finance Consultant with over 17 years of 
+      experience. Exceptional track record in all aspects of Bookkeeping & Accounting,
+       Goods & Services Tax (GST),
+       TDS, Reconciliations, ESI/PF matters and other compliances.
+      </p>
+    </div>
+    <div class="d1">
+    <img src="images/Bharat_Gulati.webp" alt="target">
+    <h4>CS Bharat Gulati | Consultant Company Secretary</h4>
+     <p>
+     Bharat Gulati is a Company Secretary in Practice having experience of more than 
+     12 years in secretarial services, holding membership with the prestigious
+      Institute of Company Secretaries of India (ICSI). With a strong background in 
+      corporate law and governance, Bharat specializes in providing comprehensive 
+      company secretarial services to clients from various industries. Bharat has
+       amassed extensive experience in the field of corporate secretarial practices
+        and has served clients ranging from small-scale enterprises to large
+         multinational corporations. With a thorough understanding of corporate 
+     laws, regulations, and compliance requirements, Bharat excels in providing 
+     strategic guidance and ensuring legal and regulatory compliance for organizations.
+     </p>
+    </div>
+  </div>
+</div>
+<h1>OUR SERVICES</h1>
+    <hr>
+    <div class="container-fluid-4">
+      <div class="container-4">
+        <div class="d4">
+         <img src="images/5.webp" alt="target">
+           <h3>New Business Registration</h3>
+           <p>Individual Proprietorship | Partnership Firm |
+             Private Limited Company | 
+             Charitable Trust | Section 8 Company (NPO)</p>
+        </div>
+        <div class="d4">
+        <img src="images/6.webp" alt="target">
+           <h3>One Stop Startup Services</h3>
+           <p>Incorporation | Registrations | Bookkeeping & Accounting | GST Compliances |
+             TDS Compliances | Income Tax Compliances |ROC Compliances |
+              Payroll Services | Auditing | Financial Analysis| MIS Reports</p>
+        </div>
+        <div class="d4">
+        <img src="images/7.webp" alt="target">
+           <h3>Bookkeeping & Accounting</h3>
+           <p>Individual Proprietors | Partnership Firm |
+             Private Limited Company | Charitable Trust | Section 8 Company (NPO)</p>
+        </div>
+        <div class="d4">
+        <img src="images/8.webp" alt="target">
+        <h3>Income Tax</h3>
+        <p>Consultancy | Tax Returns Preparation & Filing | 
+          Tax Planning | Income Tax Audits</p>
+      </div>
+      <div class="d4">
+        <img src="images/9.webp" alt="target">
+          <h3>Goods & Services Tax (GST)</h3>
+          <p>Registration | Compliances | LUT for Exporters | Replies to GST Department | GST Audit</p>
+        </div>
+        <div class="d4">
+        <img src="images/10.webp" alt="target">
+           <h3>Audit & Attestation</h3>
+           <p>Statutory Audit | Internal Audit| Income Tax u/s 44AB |
+             Income Tax Audit of NGO ((Form 10B)) | GST Audit | Independent Audit.
+         </p>
+        </div>
+        <div class="d4">
+        <img src="images/11.webp" alt="target">
+           <h3>Company Law Matters</h3>
+           <p>Formation of Company | Secretarial Compliances | 
+            ROC Mattes | XBRL Filings | Company Winding Up</p>
+        </div>
+        <div class="d4">
+        <img src="images/12.webp" alt="target">
+           <h3>Services to Charitable Trusts</h3>
+           <p>Accounting | Documentation | Donation Tracker | 
+            Statement of Donation (Form 10BD) | Income Tax Audit (Form 10B)</p>
+        </div>
+        <div class="d4">
+        <img src="images/13.webp" alt="target">
+           <h3>Certifications</h3>
+           <p>Net Worth Certificate | Fixed Asset Certificate | Capital Gain Certificate | 15CB & Etc.</p>
+        </div>
+        <div class="d4">
+        <img src="images/14.webp" alt="target">
+           <h3>Corporate Finance</h3>
+           <p>Financial Analysis| Variance Analysis | MIS Reports | Dashboards</p>
+        </div>
+        <div class="d4">
+        <img src="images/15.webp" alt="target">
+           <h3>Payroll Services</h3>
+           <p>Defining CTC Structure | Monthly Salary Computation |
+             TDS Compliances | Filing Taxes | Reimbursements | Pay slips & Etc.</p>
+        </div>
+        <div class="d4">
+        <img src="images/16.webp" alt="target">
+           <h3>Misc. Services</h3>
+           <p>PAN | TAN | Trademark Registration | Digital Signature Tokens (DSC) & Etc.</p>
+        </div>
+      </div>
+    </div>
+ </div>
+ <footer>
+  <h5>Copyright © 2023 caadityagoel.in</h5>
+  <h5>Proudly Created by Pixabulous Designs</h5>
+ </footer>
+
+</body>
+</html>
